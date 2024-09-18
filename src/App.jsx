42ef8045631c905c6d2edf74db233c3ce562632f
ptprice <11355676+ptprice@users.noreply.github.com>
2024-09-18 10:43:21 -0600

    update to App.cs, app.jsx and index.css

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@
 import { Outlet } from 'react-router-dom';
 import Nav from './components/NavTabs';
 import Footer from './components/footer';
+import './App.css';
 
 function App() {
   // The Outlet component will conditionally swap between the different pages according to the URL
